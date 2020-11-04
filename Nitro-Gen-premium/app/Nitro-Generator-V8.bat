12d969b3a96ff211ba465d1b7cababef78945c6a
achul123 <73477238+achul123@users.noreply.github.com>
2020-11-04 16:39:24 +0100

    Add files via upload

---
+++ Nitro-Gen-premium/app/Nitro-Generator-V8.bat
@@ -0,0 +1,98 @@
+ÿþ&cls
+@echo off
+title Nitro Generator By Oafu
+mode con cols=100 lines=40
+color c
+color %color%
+echo.
+echo ษอออออออออออออออออออออออออออออออออออป
+echo บ  - Nitro Generator V8 By Oafu ! - บ
+echo ศอออออออออออออออออออออออออออออออออออผ
+echo.
+echo Page 1/2. 
+echo.
+echo - [1] Start 
+echo.
+echo - [2] See logs
+echo.
+echo - [3] Colors
+echo.
+echo - [4] See Hit
+echo.
+echo - [5] Language
+echo.
+echo - [6] Next Pages
+echo.
+echo - [7] Restart
+echo.
+echo - [8] Exit
+echo.
+set /p choix= Choice : 
+echo.
+
+if "%choix%"=="1" node App_(dont_open).js
+if "%choix%"=="1" echo Gen Started at %date%, %time% >> logs.txt
+
+
+if "%choix%"=="2" cls
+if "%choix%"=="2" echo.
+if "%choix%"=="2" color a
+if "%choix%"=="2" type logs.txt
+if "%choix%"=="2" echo.
+if "%choix%"=="2" pause
+if "%choix%"=="2" start app\Nitro-Generator-V8.bat
+if "%choix%"=="2" exit
+
+if "%choix%"=="8" exit
+
+if "%choix%"=="3" echo ษออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+if "%choix%"=="3" echo บ      a = green  b = blue c = red d = purple e = yellow     บ
+if "%choix%"=="3" echo ศออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+if "%choix%"=="3" echo.
+if "%choix%"=="3" set /p color=Color (a,b,c,d,e) : 
+if "%choix%"=="3" start app\Nitro-Generator-V8.bat
+if "%choix%"=="3" exit
+
+if "%choix%"=="5" echo.
+if "%choix%"=="5" echo Choose Language
+if "%choix%"=="5" echo.
+if "%choix%"=="5" color c
+if "%choix%"=="5" echo   ษออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป
+if "%choix%"=="5" echo   บ        English(1)      Francais(2)      Spanish(3)         บ
+if "%choix%"=="5" echo   ศออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ
+
+
+if "%choix%"=="5" echo. 
+if "%choix%"=="5" echo   Choice (1/2/3) : Soon !  
+if "%choix%"=="5" echo.
+if "%choix%"=="5" pause
+if "%choix%"=="5" start app\Nitro-Generator-V8.bat
+if "%choix%"=="5" exit
+
+if "%choix%"=="7" start app\Nitro-Generator-V8.bat
+if "%choix%"=="7" exit
+
+if "%choix%"=="6" cls
+if "%choix%"=="6" echo ษอออออออออออออออออออออออออออออออออออป
+if "%choix%"=="6" echo บ  - Nitro Generator V8 By Oafu ! - บ
+if "%choix%"=="6" echo ศอออออออออออออออออออออออออออออออออออผ
+if "%choix%"=="6" echo.
+if "%choix%"=="6" echo Page 2/2. 
+if "%choix%"=="6" echo.
+if "%choix%"=="6" echo - [1] SOON
+if "%choix%"=="6" echo.
+if "%choix%"=="6" echo Choose :
+if "%choix%"=="6" echo.
+if "%choix%"=="6" pause
+if "%choix%"=="6" start app\Nitro-Generator-V8.bat
+if "%choix%"=="6" exit
+
+if "%choix%"=="4" cls
+if "%choix%"=="4" echo.
+if "%choix%"=="4" color a
+if "%choix%"=="4" type hit.txt
+if "%choix%"=="4" echo.
+if "%choix%"=="4" pause
+if "%choix%"=="4" start app\Nitro-Generator-V8.bat
+if "%choix%"=="4" exit
+pause
